408db290497770dbf8f824119c64786e4f6c9ef0
Dave Trost <davetrost@gmail.com>
2019-10-03 21:05:27 -0700

    joke routes tested

--- e2e/data-helpers.js
+++ e2e/data-helpers.js
@@ -13,7 +13,29 @@ function signupUser(user = testUser) {
     .then(({ body }) => body);
 }
 
+const joke = {
+  joke:
+    'What do you call a pirate with two eyes, two hands and two legs? - A beginner.'
+};
+
+function postJokeAsUser(user) {
+  return request
+    .post('/api/jokes')
+    .set('Authorization', user.token)
+    .send(joke)
+    .expect(200);
+}
+
+function getFromPathAsUser(path, user, status = 200) {
+  return request
+    .get(path)
+    .set('Authorization', user.token)
+    .expect(status);
+}
+
 module.exports = {
-  signupUser
+  signupUser,
+  postJokeAsUser,
+  getFromPathAsUser
 };
 
